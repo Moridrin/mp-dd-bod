c0bc669bddfb1a02c50e7c24b9a47b92cb337388
jberkvens <J.Berkvens@Triptic.nl>
2018-10-17 10:01:49 +0200

    First Event of the day added to calendar

--- shortcodes/Calendar.php
+++ shortcodes/Calendar.php
@@ -44,87 +44,87 @@ abstract class Calendar
             ['name' => 'Elmon', 'cicle' => 11, 'start' => 0],
             ['name' => 'Elralei', 'cicle' => 27, 'start' => 3],
         ];
-        $year          = 3025;
+        $year          = 2018;
         $day           = 0;
         $monthStartsOn = 1;
         ?>
         <style>
-    .lunar {
-        display: block;
-        width: 32px;
-        height: 32px;
-        background: url(https://moridrin.com/wp-content/uploads/2018/08/Phases-of-the-Moon.png);
-        text-indent: -1000px;
-        overflow: hidden;
-    }
+            .lunar {
+                display: block;
+                width: 32px;
+                height: 32px;
+                background: url(https://moridrin.com/wp-content/uploads/2018/08/Phases-of-the-Moon.png);
+                text-indent: -1000px;
+                overflow: hidden;
+            }
 
-    .lunar.phase-0 {
-        background-position: 0;
-    }
+            .lunar.phase-0 {
+                background-position: 0;
+            }
 
-    .lunar.phase-1 {
-        background-position: -128px 0;
-    }
+            .lunar.phase-1 {
+                background-position: -128px 0;
+            }
 
-    .lunar.phase-2 {
-        background-position: -256px 0;
-    }
+            .lunar.phase-2 {
+                background-position: -256px 0;
+            }
 
-    .lunar.phase-3 {
-        background-position: -384px 0;
-    }
+            .lunar.phase-3 {
+                background-position: -384px 0;
+            }
 
-    .lunar.phase-4 {
-        background-position: -512px 0;
-    }
+            .lunar.phase-4 {
+                background-position: -512px 0;
+            }
 
-    .lunar.phase-5 {
-        background-position: -640px 0;
-    }
+            .lunar.phase-5 {
+                background-position: -640px 0;
+            }
 
-    .lunar.phase-6 {
-        background-position: -768px 0;
-    }
+            .lunar.phase-6 {
+                background-position: -768px 0;
+            }
 
-    .lunar.phase-7 {
-        background-position: -896px 0;
-    }
+            .lunar.phase-7 {
+                background-position: -896px 0;
+            }
 
-    .wday {
-        width: 150px;
-        height: 180px;
-        vertical-align: top;
-        background-color: rgba(255, 255, 255, 0.2);
-    }
+            .wday {
+                width: 150px;
+                height: 180px;
+                vertical-align: top;
+                background-color: rgba(255, 255, 255, 0.2);
+            }
 
-    .mday, .note {
-        padding: 5px;
-        z-index: 100;
-        position: relative;
-        color: black;
-    }
+            .mday, .note {
+                padding: 5px;
+                z-index: 100;
+                position: relative;
+                color: black;
+            }
 
-    .moons {
-        z-index: 100;
-        position: relative;
-    }
+            .moons {
+                z-index: 100;
+                position: relative;
+            }
 
-    .wday a {
-        background-color: #9E9E9E;
-        position: absolute;
-        width: 150px;
-        height: 150px;
-    }
+            .wday a {
+                background-color: #9E9E9E;
+                position: absolute;
+                width: 150px;
+                height: 150px;
+            }
 
-    .wday a .overlay {
-        background-color: rgba(255, 255, 255, 0.4);
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-    }
-</style>
+            .wday a .overlay {
+                background-color: rgba(255, 255, 255, 0.4);
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+            }
+        </style>
         <table id="calendar" class="calendar">
             <tbody class="block">
             <?php foreach ($months as $month => $data): ?>
@@ -159,28 +159,52 @@ abstract class Calendar
                                             <td colspan="1"></td>
                                             <?php
                                         } else {
-                                            // $args  = [
-                                            //     'date_query' => [
-                                            //         [
-                                            //             'after'     => 'January 1st, 2015',
-                                            //             'before'    => 'December 31st, 2015',
-                                            //             'inclusive' => true,
-                                            //         ],
-                                            //     ],
-                                            // ];
-                                            // $query = new \WP_Query($args);
+                                            $args  = [
+                                                'date_query' => [
+                                                    [
+                                                        'after'    => $year.'-'.($month + 1).'-'.($monthDay),
+                                                        'before'    => $year.'-'.($month + 1).'-'.($monthDay + 1),
+                                                        'inclusive' => true,
+                                                    ],
+                                                ],
+                                            ];
+                                            $query = new \WP_Query($args);
+                                            $posts = $query->get_posts();
                                             ?>
-                                            <td style="width: 150px; height: 150px;">
-                                                <div class="mday">Day:<?=$monthDay?></div>
-                                                <div class="moons">
-                                                    <?php foreach ($moons as $moon => $moonData): ?>
-                                                        <?php $moonName = $moonData['name'] ?>
-                                                        <?php $moonPhase = ($day + $moonData['start']) % $moonData['cicle'] ?>
-                                                        <?php $moonPhase = intval(($moonPhase/$moonData['cicle'])*8) ?>
-                                                        <div title="<?=BaseFunctions::escape($moonName, 'attr')?>, <?=$lunarPhases[(($moonPhase/$moonData['cicle'])*7)]?>" class="lunar phase-<?=$moonPhase?>"><?=BaseFunctions::escape($moonName, 'html')?>, <?=$lunarPhases[$moonPhase]?></div>
-                                                    <?php endforeach; ?>
-                                                </div>
-                                                <div id="note-<?=BaseFunctions::escape($year, 'attr')?>-<?=BaseFunctions::escape($month, 'attr')?>-<?=BaseFunctions::escape($day, 'attr')?>" class="note" data-ymd="<?=BaseFunctions::escape($year, 'attr')?>-<?=BaseFunctions::escape($month, 'attr')?>-<?=BaseFunctions::escape($day, 'attr')?>"></div>
+                                            <td class="wday">
+                                                <?php
+                                                if (!empty($posts)) {
+                                                    $url = get_the_post_thumbnail_url($posts[0]->ID, 'thumbnail');
+                                                    ?>
+                                                    <a href="<?= get_permalink($posts[0]->ID) ?>" style="background: url(<?= $url ?>);">
+                                                        <div class="overlay"></div>
+                                                        <div class="mday">Day:<?=$monthDay?></div>
+                                                        <div class="moons">
+                                                            <?php foreach ($moons as $moon => $moonData): ?>
+                                                                <?php $moonName = $moonData['name'] ?>
+                                                                <?php $moonPhase = ($day + $moonData['start']) % $moonData['cicle'] ?>
+                                                                <?php $moonPhase = intval(($moonPhase/$moonData['cicle'])*8) ?>
+                                                                <div title="<?=BaseFunctions::escape($moonName, 'attr')?>, <?=$lunarPhases[(($moonPhase/$moonData['cicle'])*7)]?>" class="lunar phase-<?=$moonPhase?>"><?=BaseFunctions::escape($moonName, 'html')?>, <?=$lunarPhases[$moonPhase]?></div>
+                                                            <?php endforeach; ?>
+                                                        </div>
+                                                        <div id="note-<?=BaseFunctions::escape($year, 'attr')?>-<?=BaseFunctions::escape($month, 'attr')?>-<?=BaseFunctions::escape($day, 'attr')?>" class="note" data-ymd="<?=BaseFunctions::escape($year, 'attr')?>-<?=BaseFunctions::escape($month, 'attr')?>-<?=BaseFunctions::escape($day, 'attr')?>"></div>
+                                                    </a>
+                                                    <?php
+                                                } else {
+                                                    ?>
+                                                    <div class="mday">Day:<?=$monthDay?></div>
+                                                    <div class="moons">
+                                                        <?php foreach ($moons as $moon => $moonData): ?>
+                                                            <?php $moonName = $moonData['name'] ?>
+                                                            <?php $moonPhase = ($day + $moonData['start']) % $moonData['cicle'] ?>
+                                                            <?php $moonPhase = intval(($moonPhase/$moonData['cicle'])*8) ?>
+                                                            <div title="<?=BaseFunctions::escape($moonName, 'attr')?>, <?=$lunarPhases[(($moonPhase/$moonData['cicle'])*7)]?>" class="lunar phase-<?=$moonPhase?>"><?=BaseFunctions::escape($moonName, 'html')?>, <?=$lunarPhases[$moonPhase]?></div>
+                                                        <?php endforeach; ?>
+                                                    </div>
+                                                    <div id="note-<?=BaseFunctions::escape($year, 'attr')?>-<?=BaseFunctions::escape($month, 'attr')?>-<?=BaseFunctions::escape($day, 'attr')?>" class="note" data-ymd="<?=BaseFunctions::escape($year, 'attr')?>-<?=BaseFunctions::escape($month, 'attr')?>-<?=BaseFunctions::escape($day, 'attr')?>"></div>
+                                                    <?php
+                                                }
+                                                ?>
                                             </td>
                                             <?php
                                             ++$day;
